0226286a4cfb5bb19d7f1e9b2d940bd5be1ef2c5
agree-rafly <rafly.hernandes@gmail.com>
2023-03-15 23:19:06 +0700

    final

--- app/src/main/java/com/rukantala/movieapp/presentation/detail/DetailMovieFragment.kt
+++ app/src/main/java/com/rukantala/movieapp/presentation/detail/DetailMovieFragment.kt
@@ -176,25 +176,26 @@ class DetailMovieFragment : Fragment() {
     }
 
     private fun fetchReviewMovieOnLoading() {
-
+        binding.msvReview.viewState = MultiStateView.ViewState.LOADING
     }
 
     private fun fetchReviewMovieOnSuccess(data: List<ReviewEntity>) {
-        currentDataReview.addAll(data)
-        adapterReview.submitList(data)
+        if (data.isEmpty()) fetchReviewMovieOnEmpty() else {
+            currentDataReview.addAll(data)
+            adapterReview.submitList(data)
 
-        binding.msvReview.viewState = MultiStateView.ViewState.CONTENT
+            binding.msvReview.viewState = MultiStateView.ViewState.CONTENT
+        }
     }
 
     private fun fetchReviewMovieOnEmpty() {
-
+        binding.msvReview.viewState = MultiStateView.ViewState.EMPTY
     }
 
     private fun fetchReviewMovieOnError(data: BasicEntity?) {
         Toast.makeText(requireContext(), data?.message.toString() ?: "", Toast.LENGTH_SHORT)
             .show()
     }
-
 
     private fun handleStateReview(state: ReviewMovieState) {
         when (state) {
@@ -214,74 +215,80 @@ class DetailMovieFragment : Fragment() {
     }
 
     private fun fetchVideoMovieOnLoading() {
-
+        binding.msvVideo.viewState = MultiStateView.ViewState.LOADING
     }
 
     private fun fetchVideoMovieOnSuccess(data: List<VideoEntity>) {
-        val data: YouTubePlayerListener = object : YouTubePlayerListener {
-            override fun onApiChange(youTubePlayer: YouTubePlayer) {
+        if (data.isNotEmpty()) {
+            val data: YouTubePlayerListener = object : YouTubePlayerListener {
+                override fun onApiChange(youTubePlayer: YouTubePlayer) {
+
+                }
+
+                override fun onCurrentSecond(youTubePlayer: YouTubePlayer, second: Float) {
+
+                }
+
+                override fun onError(
+                    youTubePlayer: YouTubePlayer,
+                    error: PlayerConstants.PlayerError
+                ) {
+
+                }
+
+                override fun onPlaybackQualityChange(
+                    youTubePlayer: YouTubePlayer, playbackQuality: PlayerConstants.PlaybackQuality
+                ) {
+
+                }
+
+                override fun onPlaybackRateChange(
+                    youTubePlayer: YouTubePlayer, playbackRate: PlayerConstants.PlaybackRate
+                ) {
+
+                }
+
+                override fun onReady(youTubePlayer: YouTubePlayer) {
+                    youTubePlayer.loadVideo(data[0].key, 0F)
+                    youTubePlayer.pause()
+                }
+
+                override fun onStateChange(
+                    youTubePlayer: YouTubePlayer, state: PlayerConstants.PlayerState
+                ) {
+
+                }
+
+                override fun onVideoDuration(youTubePlayer: YouTubePlayer, duration: Float) {
+
+                }
+
+                override fun onVideoId(youTubePlayer: YouTubePlayer, videoId: String) {
+                    youTubePlayer.pause()
+
+                }
+
+                override fun onVideoLoadedFraction(
+                    youTubePlayer: YouTubePlayer, loadedFraction: Float
+                ) {
+
+                }
 
             }
-
-            override fun onCurrentSecond(youTubePlayer: YouTubePlayer, second: Float) {
-
-            }
-
-            override fun onError(
-                youTubePlayer: YouTubePlayer,
-                error: PlayerConstants.PlayerError
-            ) {
-
-            }
-
-            override fun onPlaybackQualityChange(
-                youTubePlayer: YouTubePlayer, playbackQuality: PlayerConstants.PlaybackQuality
-            ) {
-
-            }
-
-            override fun onPlaybackRateChange(
-                youTubePlayer: YouTubePlayer, playbackRate: PlayerConstants.PlaybackRate
-            ) {
-
-            }
-
-            override fun onReady(youTubePlayer: YouTubePlayer) {
-                youTubePlayer.loadVideo(data[0].key, 0F)
-                youTubePlayer.pause()
-            }
-
-            override fun onStateChange(
-                youTubePlayer: YouTubePlayer, state: PlayerConstants.PlayerState
-            ) {
-
-            }
-
-            override fun onVideoDuration(youTubePlayer: YouTubePlayer, duration: Float) {
-
-            }
-
-            override fun onVideoId(youTubePlayer: YouTubePlayer, videoId: String) {
-                youTubePlayer.pause()
-
-            }
-
-            override fun onVideoLoadedFraction(
-                youTubePlayer: YouTubePlayer, loadedFraction: Float
-            ) {
-
-            }
-
+            binding.youtubePlayerView.addYouTubePlayerListener(data)
+        } else {
+            fetchVideoMovieOnEmpty()
         }
-        binding.youtubePlayerView.addYouTubePlayerListener(data)
+
     }
 
     private fun fetchVideoMovieOnEmpty() {
-
+        binding.msvVideo.viewState = MultiStateView.ViewState.EMPTY
     }
 
     private fun fetchVideoMovieOnError(data: BasicEntity?) {
-
+        Toast.makeText(requireContext(), data?.message.toString() ?: "", Toast.LENGTH_SHORT)
+            .show()
     }
 
 
